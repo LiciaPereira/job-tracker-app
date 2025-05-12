e681f155ae8c4cc496c01ec9aae9e3c987ed2311
Licia Pereira <78990316+LiciaPereira@users.noreply.github.com>
2025-05-12 11:40:30 -0400

    replace the loading... with the animated spin

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -5,6 +5,13 @@ import { JSX } from "react";
 export function PrivateRoute({ children }: { children: JSX.Element }) {
   const { user, loading } = useAuth();
 
-  if (loading) return <div>Loading...</div>;
+  if (loading) {
+    return (
+      <div className="min-h-[50vh] flex items-center justify-center">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary-600"></div>
+      </div>
+    );
+  }
+
   return user ? children : <Navigate to="/" />;
 }
